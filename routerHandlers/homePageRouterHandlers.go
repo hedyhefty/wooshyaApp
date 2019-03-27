de25cf780a2f7a9843dceae92a1b2b0fc08a36bc
SHANGTONG WANG <shangtongwang@SHANGTONGdeMacBook-Air.local>
2019-03-27 11:40:46 +0900

    delete unuseful ping code in serveral files

--- routerHandlers/homePageRouterHandlers.go
+++ routerHandlers/homePageRouterHandlers.go
@@ -6,12 +6,7 @@ import (
 )
 
 func HomePage(res http.ResponseWriter, req *http.Request) {
-	err := DB.Ping()
-	if err != nil {
-		panic(err.Error())
-	} else {
-		println("I know u DB.")
-	}
+
 	fmt.Println("PPath: ", PPath)
 	fmt.Println(PPath + "/views/index.html")
 	http.ServeFile(res, req, PPath+"/views/index.html")
--- wooApp.go
+++ wooApp.go
@@ -11,6 +11,7 @@ import (
 
 var DB *sql.DB
 
+
 func init() {
 	//start the connection to the DB(mysql).
 	DB, err := sql.Open("mysql", "root:Pi3141592653@tcp(127.0.0.1:3306)/wootestdb")
