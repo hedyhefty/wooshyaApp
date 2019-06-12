09bf4a0cbb6f499f87ddfe5edcd2bf49e3b0bdde
hedyhefty <hedyhefty@gmail.com>
2019-06-12 21:07:52 +0900

    add lots of css
    optimizing the UI of the website

---
+++ views/static/cpyProfile.js
@@ -0,0 +1,7 @@
+function setProfile() {
+    var disable_target = document.getElementById("displayProfile");
+    disable_target.style.display = "none";
+
+    var enable_target = document.getElementById("settingProfile");
+    enable_target.style.display = "block";
+}
